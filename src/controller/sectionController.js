738f07a80d415cad92b330cc2cae868585d4a9d4
Sarad Subedi <subedisarad27@gmail.com>
2022-05-11 02:28:47 +0545

    regex check for section naming

--- src/controller/sectionController.js
+++ src/controller/sectionController.js
@@ -4,11 +4,17 @@ dotenv.config();
 
 exports.getSection = async (req,res) =>{ 
     try{
-        const response = await axios.get(`https://content.guardianapis.com/${req.params.section}`,{
-            params: { 'api-key' : process.env.GUARDIAN_API  } 
-        });
-        if(response){
-            res.send(response.data.response);
+        const section = /^[a-z]+(-[a-z]+)*$/.test(req.params.section);
+        if(section){
+            const response = await axios.get(`https://content.guardianapis.com/${req.params.section}`,{
+                params: { 'api-key' : process.env.GUARDIAN_API  } 
+            });
+            if(response){
+                res.send(response.data.response);
+            }
+        }
+        else{
+            res.send("Section name not in format");
         }
     }catch(error){
         res.send("Couldnot find Data");
